b1016caf526a4261bb30e28f793ad02489dc308d
Alex Foglia <alex.f1793@gmail.com>
2023-11-25 02:46:45 +0100

    Initialization i2c

--- src/drivers/mpu9265.cpp
+++ src/drivers/mpu9265.cpp
@@ -25,12 +25,61 @@ bool MPU9265::init()
         if (ioctlRes >= 0)
         {
             uint8_t zero = 0x00;
+            uint8_t one  = 0x01;
             /** Set sample rate = 8Khz (div = 0 + 1) **/
-            int writeRes = i2cWrite(MPU9265::SMPLRT_DIV, &zero, 1);
-            if (writeRes == 1)
+            int writeRes = i2cWrite(MPU9265::Register::SMPLRT_DIV, &zero, 1);
+            if (writeRes != 1)
             {
-
+                perror("Set sample rate");
+                return false;
             }
+
+            /** Reset all sensors **/
+            writeRes = i2cWrite(MPU9265::Register::PWR_MGMT_1, &zero, 1);
+            if (writeRes != 1)
+            {
+                perror("Reset all sensors");
+                return false;
+            }
+
+            /** Power management and crystal settings **/
+            writeRes = i2cWrite(MPU9265::Register::PWR_MGMT_1, &one, 1);
+            if (writeRes != 1)
+            {
+                perror("Power management");
+                return false;
+            }
+            
+            /** Write to Configuration register **/
+            writeRes = i2cWrite(MPU9265::Register::CONFIG, &zero, 1);
+            if (writeRes != 1)
+            {
+                perror("Configuration register");
+                return false;
+            }
+
+            if (!setGyroResolution(MPU9265::GyroResolution::GYRO_250_DPS))
+            {
+                perror("Set gyro resolution 250 DPS");
+                return false;
+            }
+
+            if (!setAccelResolution(MPU9265::AccelResolution::ACCEL_2G))
+            {
+                perror("Set accelerometer resolution 2G");
+                return false;
+            }
+
+            writeRes = i2cWrite(MPU9265::Register::INT_ENABLE, &one, 1);
+            if (writeRes != 1)
+            {
+                perror("Interrupt enable");
+            }
+            else
+            {
+                return true;
+            }
+            
         }
         else
         {
@@ -47,12 +96,28 @@ bool MPU9265::init()
 }
 
 
-int MPU9265::i2cWrite(uint8_t reg, uint8_t* data, uint32_t data_len)
+bool MPU9265::setGyroResolution(MPU9265::GyroResolution gyroResolution)
+{
+    uint8_t byte = static_cast<uint8_t>(gyroResolution);
+
+    return i2cWrite(MPU9265::Register::GYRO_CONFIG, &byte, 1);
+}
+
+
+bool MPU9265::setAccelResolution(MPU9265::AccelResolution accelResolution)
+{
+    uint8_t byte = static_cast<uint8_t>(accelResolution);
+
+    return i2cWrite(MPU9265::Register::ACCEL_CONFIG, &byte, 1);
+}
+
+
+int MPU9265::i2cWrite(MPU9265::Register reg, uint8_t* data, uint32_t data_len)
 {
     uint8_t* buf = new uint8_t[data_len + 1];
     memset(buf, 0x00, data_len + 1);
 
-    buf[0] = reg;
+    buf[0] = static_cast<uint8_t>(reg);
 
     for (uint32_t i = 0; i < data_len; i++)
     {
@@ -62,6 +127,7 @@ int MPU9265::i2cWrite(uint8_t reg, uint8_t* data, uint32_t data_len)
     int writeResult = write(_fd, buf, data_len);
     delete[] buf;
 
+    usleep(1000);
     return writeResult;
 }
 
--- src/drivers/mpu9265.h
+++ src/drivers/mpu9265.h
@@ -14,38 +14,57 @@ typedef struct
     int16_t gz;
 } MPU9265RawData;
 
-
-
 class MPU9265
 {
 public:
     const uint8_t I2C_ADDR     = 0x68;
 
     // Registers
-    const uint8_t PWR_MGMT_1   = 0x6B;
-    const uint8_t SMPLRT_DIV   = 0x19;
-    const uint8_t CONFIG       = 0x1A;
-    const uint8_t GYRO_CONFIG  = 0x1B;
-    const uint8_t ACCEL_CONFIG = 0x1C;
-    const uint8_t INT_ENABLE   = 0x38;
-    const uint8_t ACCEL_XOUT_H = 0x3B;
-    const uint8_t ACCEL_YOUT_H = 0x3D;
-    const uint8_t ACCEL_ZOUT_H = 0x3F;
-    const uint8_t TEMP_OUT_H   = 0x41;
-    const uint8_t GYRO_XOUT_H  = 0x43;
-    const uint8_t GYRO_YOUT_H  = 0x45;
-    const uint8_t GYRO_ZOUT_H  = 0x47;
+    enum class Register
+    {
+        PWR_MGMT_1   = 0x6B,
+        SMPLRT_DIV   = 0x19,
+        CONFIG       = 0x1A,
+        GYRO_CONFIG  = 0x1B,
+        ACCEL_CONFIG = 0x1C,
+        INT_ENABLE   = 0x38,
+        ACCEL_XOUT_H = 0x3B,
+        ACCEL_YOUT_H = 0x3D,
+        ACCEL_ZOUT_H = 0x3F,
+        TEMP_OUT_H   = 0x41,
+        GYRO_XOUT_H  = 0x43,
+        GYRO_YOUT_H  = 0x45,
+        GYRO_ZOUT_H  = 0x47
+    };
+
+    enum class GyroResolution : uint8_t
+    {
+        GYRO_250_DPS  = 0x00,
+        GYRO_500_DPS  = 0x08,
+        GYRO_1000_DPS = 0x10,
+        GYRO_2000_DPS = 0x18
+    };
+
+    enum class AccelResolution : uint8_t
+    {
+        ACCEL_2G  = 0x00,
+        ACCEL_4G  = 0x08,
+        ACCEL_8G  = 0x10,
+        ACCEL_16G = 0x18
+    };
 
     MPU9265();
 
     bool init();
+    bool setGyroResolution(MPU9265::GyroResolution gyroResolution);
+    bool setAccelResolution(MPU9265::AccelResolution accelResolution);
 
 private:
 
     int _fd;
     MPU9265RawData _rawData;
 
-    int i2cWrite(uint8_t reg, uint8_t* data, uint32_t data_len);
+    int i2cWrite(MPU9265::Register reg, uint8_t* data, uint32_t data_len);
 
 };
 
